8d639491cd4d51b3c7f72c47d9ad7b3f9817e4ec
kyrenmearr <124427773+kyrenmearr@users.noreply.github.com>
2023-06-30 12:32:40 +0800

    Update models.py

--- website/models.py
+++ website/models.py
@@ -11,4 +11,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True) #unique means that no user can create an account with an existing email in the database
     password = db.Column(db.String(150))
-    first_name = db.column(db.String(150))
+    first_name = db.Column(db.String(150))
