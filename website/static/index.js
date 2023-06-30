38487bd48f5263edb6183ebebef2896d9465c915
kyrenmearr <kyren.cabellon@go.team>
2023-06-30 14:51:53 +0800

    Completed the project but with some minor issue

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,8 @@
-fucntion deleteNote(noteId) { // noteId is passed in from the HTML
-    fetch('/delete-note', { // fetch is a built in function in JS
-        method: 'POST', // POST is a method of sending data to the server
-        body: JSON.stringify({ noteId: noteId }) // JSON.stringify converts the data into a string
-    }).then((_res) => { // _res is the response from the server
-        window.location.href = '/'; // redirect to the home page
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+      method: "POST",
+      body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+      window.location.href = "/";
     });
-}
+  }
