4b5d49a57b69407e1b4742692ed282ce727a9027
kyrenmearr <kyren.cabellon@go.team>
2023-06-30 14:38:46 +0800

    Deleting notes

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from . import db ##This will import the database from the __init__.py file
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -55,7 +55,9 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+
+            login_user(new_user, remember=True) #This will login the new user
+
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
             
--- website/models.py
+++ website/models.py
@@ -2,14 +2,17 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000)) #This is the actual note
+    data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #This will link the note to the user and their details
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True) #unique means that no user can create an account with an existing email in the database
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
