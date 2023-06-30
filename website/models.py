eb844666ddd702eefb0da121abe8f02deb6691b2
kyrenmearr <kyren.cabellon@go.team>
2023-06-30 14:14:20 +0800

    Initial creating notes

--- website/models.py
+++ website/models.py
@@ -6,6 +6,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000)) #This is the actual note
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #This will link the note to the user and their details
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
--- website/views.py
+++ website/views.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required #This will make sure that the user is logged in before accessing the home page
 def home():
+    if request.method == 'POST':
+        note = request.form.get('note') #This will get the note from the form
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id) #This will add the note to the database
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
     return render_template("home.html", user=current_user)
